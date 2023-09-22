76d8af6d283e192d988b9f14afae53a6b828a689
SUMAN PAUL CHOUDHURY <34674003+spcCodes@users.noreply.github.com>
2023-09-22 23:41:12 +0530

    Data Transformation codes added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -19,6 +21,18 @@ STAGE_NAME = "Data Validation stage"
 try:
     logger.info(f"Running {STAGE_NAME} !")
     pipeline = DataValidationTrainingPipeline()
+    pipeline.main()
+    logger.info(f"{STAGE_NAME} completed !")
+except Exception as e:
+    logger.error(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+
+try:
+    logger.info(f"Running {STAGE_NAME} !")
+    pipeline = DataTransformationTrainingPipeline()
     pipeline.main()
     logger.info(f"{STAGE_NAME} completed !")
 except Exception as e:
---
+++ src/mlProject/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,40 @@
+from mlProject.config.configuration import ConfigurationManager
+from mlProject.components.data_transformation import DataTransformation
+from mlProject import logger
+from pathlib import Path
+
+STAGE_NAME = "Data Transformation"
+
+class DataTransformationTrainingPipeline:
+    
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        try:
+            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
+                status = f.read().split(" ")[-1]
+
+            if status == "True":
+                config = ConfigurationManager()
+                data_transformation_config = config.get_data_transformation_config()
+                data_transformation = DataTransformation(config=data_transformation_config)
+                data_transformation.train_test_spliting()
+
+            else:
+                raise Exception("You data schema is not valid")
+
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    try:
+        logger.info("Stage: {}".format(STAGE_NAME))
+        pipeline = DataTransformationTrainingPipeline()
+        pipeline.main()
+        logger.info("Stage completed successfully")
+    except Exception as e:
+        logger.error(e)
+        logger.error("Stage Failed")
+        raise e
